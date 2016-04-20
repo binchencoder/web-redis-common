62cf9062d7ee8710e7bdb9ec8abdbfb0bcda18ce
chenbin <chenbin@jingoal.com>
2016-04-20 11:49:42 +0800

    add test case

---
+++ src/test/java/com/jingoal/web/redis/BaseTest.java
@@ -0,0 +1,15 @@
+package com.jingoal.web.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations = "classpath:root-context.xml")
+public class BaseTest extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	public RedisTemplate<String, String> template; // inject the template as
+													// ListOperations
+	
+}
